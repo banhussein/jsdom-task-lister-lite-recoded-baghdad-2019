f3440dbd3cb5d8f69abefd6798f6a08c7ff3965b
ban hussein <banhussien97@gmail.com>
2019-09-21 17:39:02 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -8,9 +8,10 @@ let input =document.getElementById('new-task-description');
 let list = document.getElementById('tasks');
 
  function todo(event){
-    event.preventDefault();
+    event.preventDefault();  
+    let inputVal = input.value;
+
    let list = document.getElementById('tasks');
-   let inputVal = input.value;
     task.innerText = inputVal;
     list.appendChild(task);
     
