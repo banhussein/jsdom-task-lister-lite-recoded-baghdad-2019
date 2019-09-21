23e82154b905715989d844ba173434e0483533dc
ban hussein <banhussien97@gmail.com>
2019-09-21 17:33:36 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ let list = document.getElementById('tasks');
 let list = document.getElementById('tasks');
     task.innerText = inputVal;
     list.appendChild(task);
+    list.appendChild(task);
 
  }
 
