e916ae75b4e7270da8141d3c068813e1a0d7fc84
ban hussein <banhussien97@gmail.com>
2019-09-21 17:34:16 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  
+      console.log('tasks');
+
 
 });
 let input =document.getElementById('new-task-description');
@@ -8,7 +9,7 @@ let list = document.getElementById('tasks');
  function todo(event){
    event.preventDefault();
    let list = document.getElementById('tasks');
-let list = document.getElementById('tasks');
+
     task.innerText = inputVal;
     list.appendChild(task);
     
